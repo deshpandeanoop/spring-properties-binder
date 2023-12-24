d74d5da4453086f16b37fddf11ee8ff1c9921b36
Anoop Deshpande <anoop.bhardwaja@gmail.com>
2023-12-24 15:40:52 +0530

    Initial commit

---
+++ src/main/java/com/explore/properties/binder/SpringPropertiesBinderApplication.java
@@ -0,0 +1,13 @@
+package com.explore.properties.binder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesBinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertiesBinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explore/properties/binder/SpringPropertiesBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.properties.binder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
